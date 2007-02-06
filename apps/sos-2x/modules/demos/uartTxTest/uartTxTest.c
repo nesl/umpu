f148c766a8b501ad8a16b26ad0af403c5c102da3
ram <ram@eac192a1-7224-0410-95d1-9f1386daca0f>
2007-02-06 01:32:03 +0000

    uartTxTest module bug fix

--- apps/sos-2x/modules/demos/uartTxTest/uartTxTest.c
+++ apps/sos-2x/modules/demos/uartTxTest/uartTxTest.c
@@ -20,7 +20,7 @@
 #include "uartTxTest.h"
 
 #ifdef SOS_SFI
-#define UART_TX_TEST_DOM_ID  SFI_DOM0
+#define UART_TX_TEST_DOM_ID  SFI_DOM2
 #endif
 
 #define UART_TX_TIMER_INTERVAL	20L
@@ -67,7 +67,7 @@ static mod_header_t uartTxTest_mod_header SOS_MODULE_HEADER = {
 	.code_id        = ehtons(DFLT_APP_ID0),
 #ifdef SOS_SFI
 	.dom_id         = UART_TX_TEST_DOM_ID,
-	.module_handler = (msg_handler_t)SFI_MOD_TABLE_ENTRY_LOC(UART_TX_TEST_DOM_ID, 0),
+	.module_handler = (msg_handler_t)SFI_JMP_TABLE_FUNC(UART_TX_TEST_DOM_ID, 0),
 #else
 	.module_handler = uartTxTest_msg_handler,
 #endif
