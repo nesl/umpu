7d2c83e8486366ad85d08527b2e5b4ea2711504b
akhi3030 <akhi3030@eac192a1-7224-0410-95d1-9f1386daca0f>
2007-02-15 22:36:58 +0000

    added data_col with R code for analysis

---
+++ data_col/analysis.R
@@ -0,0 +1,52 @@
+analysis = function(fileName,bitNumber){
+
+  # Constants for detecting the initial pattern 
+  STEP1 = 85
+  STEP2 = 170
+  EVENT = 1
+  
+  # Read the data from the file
+  file_data = read.table(fileName,sep = "\t",header = TRUE)
+
+  # List of variables used in this function
+  time_stamp = 0
+  value = 0
+  monitor = 0
+  start_point = 0
+  nevents = 0
+  events_list = 0
+  avg = 0
+
+  # Assigning values to the variables
+  time_stamp = file_data[,1]
+  value = file_data[,2]
+  # Selecting the bit from the value
+  monitor = (value %/% (2 ^ bitNumber)) %% 2
+
+  # Detecting the initial pattern
+  for(i in 2:length(value)) {
+    if(value[i-1] == STEP1 & value[i] == STEP2){
+      print("found pattern");
+      start_point = i + 1
+      break;
+    }
+  }
+
+  print(monitor)
+
+  # Constructing events_list
+  # events_list contains the time period that an event stays high
+  for(i in start_point:(length(value) - 1)) {
+    if(monitor[i] == EVENT) {
+      print("found event")
+      events_list[nevents + 1] = time_stamp[i+1] - time_stamp[i]
+      nevents = nevents + 1
+    }
+  }
+
+  print(events_list)
+  print(length(events_list))
+  print("avg and dev")
+  print(ave(events_list))
+  print(sd(events_list))
+}
